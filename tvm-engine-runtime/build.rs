10013ac94fa0395658cec22b2213a65f843fdff6
CharlesLiu <charles.liu@upblocks.io>
2023-02-28 07:44:43 +0000

    update protobuf include directory

--- tvm-engine-runtime/build.rs
+++ tvm-engine-runtime/build.rs
@@ -1,15 +1,17 @@
-fn main() {
-    #[cfg(feature = "build_as_xtop_lib")]
-    let out_dir = std::env::var("RUST_COMPILE_C_OUTPUT_DIR").unwrap();
+#[cfg(not(feature = "build_as_xtop_lib"))]
+fn main() {}
 
-    #[cfg(not(feature = "build_as_xtop_lib"))]
-    let out_dir = std::env::var("OUT_DIR").unwrap();
+#[cfg(feature = "build_as_xtop_lib")]
+fn main() {
+    let out_dir = std::env::var("RUST_COMPILE_C_OUTPUT_DIR").unwrap();
 
     println!("cargo:rerun-if-changed=../tvm-c-api/");
     cc::Build::new()
         .cpp(true)
         .flag("-std=c++11")
+        .flag("-Wno-unused-parameter") // disable warning from google/protobuf
         .include("../tvm-c-api/")
+        .include("../../xdepends/include/") // to include src/xtopcom/xdepends/include get protobuf
         .file("../tvm-c-api/tvm_import_instance.cpp")
         .file("../tvm-c-api/protobuf_types/pbasic.pb.cc")
         .file("../tvm-c-api/protobuf_types/pparameters.pb.cc")
